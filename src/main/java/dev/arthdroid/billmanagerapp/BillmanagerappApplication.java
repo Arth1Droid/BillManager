e891ad2797807c46d9352c7d21b9ffb127761088
ArthDroid1 <barbosarthur7@gmail.com>
2026-06-21 17:37:28 -0300

    start project

---
+++ src/main/java/dev/arthdroid/billmanagerapp/BillmanagerappApplication.java
@@ -0,0 +1,13 @@
+package dev.arthdroid.billmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillmanagerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillmanagerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arthdroid/billmanagerapp/BillmanagerappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arthdroid.billmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillmanagerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
