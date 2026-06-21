b6c826a1949e70ce9d330541c066022e0d989ea9
ArthDroid1 <barbosarthur7@gmail.com>
2026-06-21 19:00:54 -0300

    feat: implement entity auditing with timestamps

---
+++ src/main/java/dev/arthdroid/billmanagerapp/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package dev.arthdroid.billmanagerapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
